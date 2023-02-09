c14c8cac31c7fee7d873037bba021cb84371e492
liuyang <poteryung@gmail.com>
2023-02-09 23:05:23 +0800

    feat: test

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Buttons = ({ onClick }) => {
-  return <button onClick={onClick}>Click</button>;
+  return <button onClick={onClick}>Click Me</button>;
 };
 
 export default Buttons;
